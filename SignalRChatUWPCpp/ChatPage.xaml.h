71e1e71194cdbf2290a1275dbbb2055d5d6b8fc6
Pawel Kadluczka <moozzyk@gmail.com>
2017-01-16 18:20:36 -0800

    Initial commit

---
+++ SignalRChatUWPCpp/ChatPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// ChatPage.xaml.h
+// Declaration of the ChatPage class
+//
+
+#pragma once
+
+#include "ChatPage.g.h"
+
+namespace SignalRChatUWPCpp
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class ChatPage sealed
+	{
+	public:
+		ChatPage();
+	};
+}
